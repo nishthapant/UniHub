0e2d7b8063a07bcdb2b85de99b4690fe689eb014
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-20 22:40:55 -0600

    update mc_config

--- scrapers/config.js
+++ scrapers/config.js
@@ -3,6 +3,7 @@ const config = {
         ROOT :'https://www.mcgill.ca',
         SLUG : 'https://www.mcgill.ca/study/2020-2021/courses/search?page=',
         DIV : '#block-system-main > div > div > div > div.view-content',
+        SUBJECT_DIV: 'span.views-field.views-field-field-dept-code  span',
         FILE : 'mcgill.txt',
         PAGES : 541
     },
