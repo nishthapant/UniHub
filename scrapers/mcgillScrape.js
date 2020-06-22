7b8ee5f38e3cbf13f1d2e7c277f10f9e29a3eaae
nishthapant <48161605+nishthapant@users.noreply.github.com>
2020-06-22 01:33:48 -0600

    introduce classes in mcscraper

--- scrapers/mcgillScrape.js
+++ scrapers/mcgillScrape.js
@@ -3,13 +3,12 @@ const cheerio = require('cheerio');
 const fs = require('fs') ;
 const config = require("./config");
 
-
-
 class mcgillScrape {
 
   constructor(){
     this.root = config.mcgill.ROOT;
     this.div = config.mcgill.DIV;
+    this.subDiv = config.mcgill.SUBJECT_DIV;
     this.slug = config.mcgill.SLUG;
     this.pages = config.mcgill.PAGES;
     this.file = config.mcgill.FILE;
@@ -19,18 +18,27 @@ class mcgillScrape {
   async scrape(url){
       while(true){
           try{
-              let pageHTML = await axios.get(url);
-              let $ = cheerio.load(pageHTML.data);
+            let pageHTML = await axios.get(url);
+            let $ = cheerio.load(pageHTML.data);
 
-              var classConfig = this;
-              let courseObjects = $(classConfig.div).children();
+            var classConfig = this;
+            let courseObjects = $(classConfig.div).children();
+            // let courseObjects = $(this.div).children();
 
-              courseObjects.find('a').each(function(){
-                  let course = {};
-                  course.title = $(this).text();
-                  course.url = classConfig.root+$(this).attr('href');
-                  classConfig.catalogue.push(course);
-              })
+            //   courseObjects.find('a').each(function(){
+            //       let course = {};
+            //       course.title = $(this).text();
+            //       course.url = classConfig.root+$(this).attr('href');
+            //       classConfig.catalogue.push(course);
+            //   })
+
+            courseObjects.each(function(){
+                let course = {};
+                course.title = $($(this).find('a')).text();
+                course.url =  classConfig.root + $($(this).find('a')).attr('href');
+                course.subject =  $($(this).find(classConfig.subDiv)).text();
+                classConfig.catalogue.push(course);
+            })
 
               return true;
           }
